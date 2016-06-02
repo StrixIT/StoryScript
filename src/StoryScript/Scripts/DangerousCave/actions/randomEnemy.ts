3f39ef87de6b33de5630a6cf80156a2329e793e2
Rutger Schurgers <rutger@strixit.com>
2016-06-02 12:41:24 +0200

    fix random enemy

--- src/StoryScript/Scripts/DangerousCave/actions/randomEnemy.ts
+++ src/StoryScript/Scripts/DangerousCave/actions/randomEnemy.ts
@@ -2,12 +2,30 @@
     export function RandomEnemy(game: Game) {
         var enemies = game.definitions.enemies;
         var enemyCount = 0;
+        var randomEnemy = null;
 
         for (var n in enemies) {
             enemyCount++;
         }
 
-        var randomEnemy = enemies[game.rollDice('1d' + enemyCount) - 1]();
+        var enemyToGet = game.rollDice('1d' + enemyCount) - 1;
+        var index = 0;
+
+        for (var n in enemies) {
+            index++;
+
+            if (index == enemyToGet) {
+                randomEnemy = enemies[n]();
+                break;
+            }
+        }
+
+        randomEnemy.items = randomEnemy.items || [];
+
+        for (var n in randomEnemy.items) {
+            StoryScript.definitionToObject(randomEnemy.items[n]);
+        }
+
         game.currentLocation.enemies.push(randomEnemy);
 
         return randomEnemy;
--- src/StoryScript/wwwroot/js/game.js
+++ src/StoryScript/wwwroot/js/game.js
@@ -1334,10 +1334,23 @@ var DangerousCave;
         function RandomEnemy(game) {
             var enemies = game.definitions.enemies;
             var enemyCount = 0;
+            var randomEnemy = null;
             for (var n in enemies) {
                 enemyCount++;
             }
-            var randomEnemy = enemies[game.rollDice('1d' + enemyCount) - 1]();
+            var enemyToGet = game.rollDice('1d' + enemyCount) - 1;
+            var index = 0;
+            for (var n in enemies) {
+                index++;
+                if (index == enemyToGet) {
+                    randomEnemy = enemies[n]();
+                    break;
+                }
+            }
+            randomEnemy.items = randomEnemy.items || [];
+            for (var n in randomEnemy.items) {
+                StoryScript.definitionToObject(randomEnemy.items[n]);
+            }
             game.currentLocation.enemies.push(randomEnemy);
             return randomEnemy;
         }
