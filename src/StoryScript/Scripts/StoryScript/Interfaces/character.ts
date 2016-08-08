ed6f31280b6cfcf40ce8101f8b2636e8466ed6cc
Rutger Schurgers <rutger@strixit.com>
2016-08-08 17:42:00 +0200

    Fixes for equipping

--- src/StoryScript/Scripts/StoryScript/Interfaces/character.ts
+++ src/StoryScript/Scripts/StoryScript/Interfaces/character.ts
@@ -8,6 +8,15 @@
         items: StoryScript.ICollection<StoryScript.IItem>;
 
         equipment: {
+            head?: IItem,
+            body: IItem,
+            hands?: IItem,
+            leftHand?: IItem,
+            leftRing?: IItem,
+            rightHand?: IItem,
+            rightRing?: IItem,
+            legs?: IItem,
+            feet?: IItem
         }
     }
 }
