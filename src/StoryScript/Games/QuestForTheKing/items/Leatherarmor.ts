4c93f12ea00d7d892b79b5afd99614ae5279e26b
Fabian <info@fasoldgames.com>
2016-08-25 14:40:00 +0200

    items

--- src/StoryScript/Games/QuestForTheKing/items/Leatherarmor.ts
+++ src/StoryScript/Games/QuestForTheKing/items/Leatherarmor.ts
@@ -1,11 +1,10 @@
 ﻿module QuestForTheKing.Items {
-    export function Leatherarmor(): IItem {
+    export function Leatherarmor(): StoryScript.IItem {
         return {
             name: 'Leather Armor',
             damage: '2',
             equipmentType: StoryScript.EquipmentType.Body,
-            dayAvailable: 1,
-            arcane: false
+            dayAvailable: 1
         }
     }
 }
