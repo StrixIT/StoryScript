33a8808078f5b5a8e2933214ab2f81bd9bc87f5c
Rutger Schurgers <rutger@strixit.com>
2016-10-05 16:57:36 +0200

    Add completed property to locations

--- src/StoryScript/Games/QuestForTheKing/interfaces/compiledLocation.ts
+++ src/StoryScript/Games/QuestForTheKing/interfaces/compiledLocation.ts
@@ -2,5 +2,6 @@
     export interface ICompiledLocation extends StoryScript.ICompiledLocation {
         enemies?: StoryScript.ICollection<IEnemy>;
         items?: StoryScript.ICollection<IItem>;
+        completed?: boolean;
     }
 }
