f989759fe3658f5c140ab2e8eca31c399e541bdd
Fabian <info@fasoldgames.com>
2016-10-27 09:12:52 +0200

    Added Lockpicks & Platemail.

---
+++ src/StoryScript/Games/QuestForTheKing/items/Lockpicks.ts
@@ -0,0 +1,11 @@
+﻿module QuestForTheKing.Items {
+    export function Lockpicks(): IItem {
+        return {
+            name: 'Lockpicks',            
+            equipmentType: StoryScript.EquipmentType.LeftHand,
+            value: 5,
+            attackText: 'You use the lockpicks',
+            itemClass: Class.Rogue
+        }
+    }
+}
