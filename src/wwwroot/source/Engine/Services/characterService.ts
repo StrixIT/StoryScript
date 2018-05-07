c75319c424a0010c180cb2b3d74c8ab68110549c
Rutger Schurgers <rutger@strixit.com>
2018-05-07 13:12:27 +0200

    Move character methods to service

--- src/wwwroot/source/Engine/Services/characterService.ts
+++ src/wwwroot/source/Engine/Services/characterService.ts
@@ -3,50 +3,58 @@
         getSheetAttributes(): string[];
         getCreateCharacterSheet(): ICreateCharacter;
         createCharacter(game: IGame, characterData: any): ICharacter;
+        canEquip(item: IItem): boolean;
+        equipItem(item: IItem): void;
+        unequipItem(item: IItem): void;
+        isSlotUsed(slot: string): boolean;
+        dropItem(item: IItem): void;
+        questStatus(quest: IQuest): string;
     }
 }
 
 namespace StoryScript {
     export class CharacterService implements ng.IServiceProvider, ICharacterService {
-        private dataService: IDataService;
-        private rules: IRules;
-
-        constructor(dataService: IDataService, rules: IRules) {
+        constructor(private _dataService: IDataService, private _game: IGame, private _rules: IRules) {
             var self = this;
-            self.dataService = dataService;
-            self.rules = rules;
         }
 
-        public $get(dataService: IDataService, rules: IRules): ICharacterService {
+        public $get(dataService: IDataService, game: IGame, rules: IRules): ICharacterService {
             var self = this;
-            self.dataService = dataService;
-            self.rules = rules;
+            self._dataService = dataService;
+            self._game = game;
+            self._rules = rules;
 
             return {
                 getSheetAttributes: self.getSheetAttributes,
                 getCreateCharacterSheet: self.getCreateCharacterSheet,
-                createCharacter: self.createCharacter
+                createCharacter: self.createCharacter,
+                canEquip: self.canEquip,
+                equipItem: self.equipItem,
+                unequipItem: self.unequipItem,
+                isSlotUsed: self.isSlotUsed,
+                dropItem: self.dropItem,
+                questStatus: self.questStatus
             };
         }
 
         getSheetAttributes = (): string[] => {
             var self = this;
-            return self.rules.getSheetAttributes();
+            return self._rules.getSheetAttributes();
         }
 
         getCreateCharacterSheet = (): ICreateCharacter => {
             var self = this;
-            return self.rules.getCreateCharacterSheet();
+            return self._rules.getCreateCharacterSheet();
         }
 
         createCharacter = (game: IGame, characterData: ICreateCharacter): ICharacter => {
             var self = this;
-            var character = self.dataService.load<ICharacter>(StoryScript.DataKeys.CHARACTER);
+            var character = self._dataService.load<ICharacter>(StoryScript.DataKeys.CHARACTER);
 
             if (isEmpty(character)) {
 
                 var self = this;
-                character = self.rules.createCharacter(game, characterData);
+                character = self._rules.createCharacter(game, characterData);
 
                 characterData.steps.forEach(function (step) {
                     if (step.questions) {
@@ -73,7 +81,109 @@ namespace StoryScript {
 
             return character;
         }
+
+        canEquip = (item: IItem): boolean => {
+            return item.equipmentType != StoryScript.EquipmentType.Miscellaneous;
+        }
+
+        equipItem = (item: IItem): void => {
+            var self = this;
+
+            var equipmentTypes = Array.isArray(item.equipmentType) ? <EquipmentType[]>item.equipmentType : [<EquipmentType>item.equipmentType];
+
+            for (var n in equipmentTypes) {
+                var type = self.getEquipmentType(equipmentTypes[n]);
+                self.unequip(type);
+            }
+
+            if (self._rules.beforeEquip) {
+                if (!self._rules.beforeEquip(self._game, self._game.character, item)) {
+                    return;
+                }
+            }
+
+            if (item.equip) {
+                if (!item.equip(item, self._game)) {
+                    return;
+                }
+            }
+
+            for (var n in equipmentTypes) {
+                var type = self.getEquipmentType(equipmentTypes[n]);
+                self._game.character.equipment[type] = item;
+            }
+
+            self._game.character.items.remove(item);
+        }
+
+        unequipItem = (item: IItem): void => {
+            var self = this;
+            var equipmentTypes = Array.isArray(item.equipmentType) ? <EquipmentType[]>item.equipmentType : [<EquipmentType>item.equipmentType];
+
+            for (var n in equipmentTypes) {
+                var type = self.getEquipmentType(equipmentTypes[n]);
+                self.unequip(type);
+            }
+        }
+
+        isSlotUsed = (slot: string): boolean => {
+            var self = this;
+
+            if (self._game.character) {
+                return self._game.character.equipment[slot] !== undefined;
+            }
+
+            return false;
+        }
+
+        dropItem = (item: IItem): void => {
+            var self = this;
+            self._game.character.items.remove(item);
+            self._game.currentLocation.items.push(item);
+        }
+
+        questStatus = (quest: IQuest): string => {
+            var self = this;
+            return typeof quest.status === 'function' ? (<any>quest).status(self._game, quest, quest.checkDone(self._game, quest)) : quest.status;
+        }
+
+        private unequip(type: string, currentItem?: IItem) {
+            var self = this;
+            var equippedItem = self._game.character.equipment[type];
+
+            if (equippedItem) {
+                if (Array.isArray(equippedItem.equipmentType) && !currentItem) {
+                    for (var n in equippedItem.equipmentType) {
+                        var type = self.getEquipmentType(equippedItem.equipmentType[n]);
+                        self.unequip(type, equippedItem);
+                    }
+                }
+
+                if (self._rules.beforeUnequip) {
+                    if (!self._rules.beforeUnequip(self._game, self._game.character, equippedItem)) {
+                        return;
+                    }
+                }
+
+                if (equippedItem.unequip) {
+                    if (!equippedItem.unequip(equippedItem, self._game)) {
+                        return;
+                    }
+                }
+
+                if (equippedItem && !isNaN( equippedItem.equipmentType) && self._game.character.items.indexOf(equippedItem) === -1) {
+                    self._game.character.items.push(equippedItem);
+                }
+
+                self._game.character.equipment[type] = null;
+            }
+        }
+
+        private getEquipmentType = (slot: StoryScript.EquipmentType) => {
+            var type = StoryScript.EquipmentType[slot];
+            return type.substring(0, 1).toLowerCase() + type.substring(1);
+        }
     }
 
-    CharacterService.$inject = ['dataService', 'rules'];
+    CharacterService.$inject = ['dataService', 'game', 'rules'];
 }
--- src/wwwroot/source/UI/Components/CharacterSheet/CharacterSheetController.ts
+++ src/wwwroot/source/UI/Components/CharacterSheet/CharacterSheetController.ts
@@ -2,73 +2,38 @@ namespace StoryScript {
     export class CharacterSheetController {
         constructor(private _scope: ng.IScope, private _characterService: ICharacterService, private _rules: IRules, private _game: IGame, private _texts: IInterfaceTexts) {
             var self = this;
-            self.character = self._game.character;
+            self.game = self._game;
             self.texts = self._texts;
             self.displayCharacterAttributes = self._characterService.getSheetAttributes();
         }
 
-        character: ICharacter;
+        game: IGame;
         texts: IInterfaceTexts;
         displayCharacterAttributes: string[];
 
         canEquip = (item: IItem): boolean => {
-            return item.equipmentType != StoryScript.EquipmentType.Miscellaneous;
+            var self = this;
+            return self._characterService.canEquip(item);
         }
 
-        equipItem = (item: IItem) => {
+        equipItem = (item: IItem): void => {
             var self = this;
-
-            var equipmentTypes = Array.isArray(item.equipmentType) ? <EquipmentType[]>item.equipmentType : [<EquipmentType>item.equipmentType];
-
-            for (var n in equipmentTypes) {
-                var type = self.getEquipmentType(equipmentTypes[n]);
-                self.unequip(type);
-            }
-
-            if (self._rules.beforeEquip) {
-                if (!self._rules.beforeEquip(self._game, self._game.character, item)) {
-                    return;
-                }
-            }
-
-            if (item.equip) {
-                if (!item.equip(item, self._game)) {
-                    return;
-                }
-            }
-
-            for (var n in equipmentTypes) {
-                var type = self.getEquipmentType(equipmentTypes[n]);
-                self._game.character.equipment[type] = item;
-            }
-
-            self._game.character.items.remove(item);
+            self._characterService.equipItem(item);
         }
 
-        unequipItem = (item: IItem) => {
+        unequipItem = (item: IItem): void => {
             var self = this;
-            var equipmentTypes = Array.isArray(item.equipmentType) ? <EquipmentType[]>item.equipmentType : [<EquipmentType>item.equipmentType];
-
-            for (var n in equipmentTypes) {
-                var type = self.getEquipmentType(equipmentTypes[n]);
-                self.unequip(type);
-            }
+            self._characterService.unequipItem(item);
         }
 
-        isSlotUsed = (slot: string) => {
+        isSlotUsed = (slot: string): boolean => {
             var self = this;
-
-            if (self._game.character) {
-                return self._game.character.equipment[slot] !== undefined;
-            }
-
-            return false;
+            return self._characterService.isSlotUsed(slot);
         }
 
         dropItem = (item: IItem): void => {
             var self = this;
-            self._game.character.items.remove(item);
-            self._game.currentLocation.items.push(item);
+            self._characterService.dropItem(item);
             self._scope.$emit('refreshCombine');
         }
 
@@ -89,44 +54,7 @@ namespace StoryScript {
 
         questStatus = (quest: IQuest): string => {
             var self = this;
-            return typeof quest.status === 'function' ? (<any>quest).status(self._game, quest, quest.checkDone(self._game, quest)) : quest.status;
-        }
-
-        private unequip(type: string, currentItem?: IItem) {
-            var self = this;
-            var equippedItem = self._game.character.equipment[type];
-
-            if (equippedItem) {
-                if (Array.isArray(equippedItem.equipmentType) && !currentItem) {
-                    for (var n in equippedItem.equipmentType) {
-                        var type = self.getEquipmentType(equippedItem.equipmentType[n]);
-                        self.unequip(type, equippedItem);
-                    }
-                }
-
-                if (self._rules.beforeUnequip) {
-                    if (!self._rules.beforeUnequip(self._game, self._game.character, equippedItem)) {
-                        return;
-                    }
-                }
-
-                if (equippedItem.unequip) {
-                    if (!equippedItem.unequip(equippedItem, self._game)) {
-                        return;
-                    }
-                }
-
-                if (equippedItem && equippedItem.equipmentType && self._game.character.items.indexOf(equippedItem) === -1) {
-                    self._game.character.items.push(equippedItem);
-                }
-
-                self._game.character.equipment[type] = null;
-            }
-        }
-
-        private getEquipmentType = (slot: StoryScript.EquipmentType) => {
-            var type = StoryScript.EquipmentType[slot];
-            return type.substring(0, 1).toLowerCase() + type.substring(1);
+            return self._characterService.questStatus(quest);
         }
     }
 
