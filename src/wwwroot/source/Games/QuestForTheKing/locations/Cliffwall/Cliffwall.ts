9e588d5e2b20f59c25afa85c21a5205f81138a3f
Rutger Schurgers <rutger@strixit.com>
2018-05-20 21:13:19 +0200

    Update another location

--- src/wwwroot/source/Games/QuestForTheKing/locations/Cliffwall/Cliffwall.ts
+++ src/wwwroot/source/Games/QuestForTheKing/locations/Cliffwall/Cliffwall.ts
@@ -14,7 +14,6 @@
             ],
             enemies: [
                 Enemies.Twoheadedwolf
-
             ]
         }
     }
--- src/wwwroot/source/Games/QuestForTheKing/locations/Oceanshrine/Oceanshrine.ts
+++ src/wwwroot/source/Games/QuestForTheKing/locations/Oceanshrine/Oceanshrine.ts
@@ -6,12 +6,20 @@
                 {
                     name: 'Back to the Map',
                     target: Locations.Quest1map3
-                },     
-                 {
-                    name: 'Touch the Altar',
-                    target: Locations.Touchingaltar
-                }            
-
+                }        
+            ],
+            items: [
+                // Todo: should this be inactive and only show for the wizard when touching the altar?
+                Items.Magicring,            
+            ],
+            actions: [
+                {
+                    text: 'Touch the Altar',
+                    execute: (game: IGame) => {
+                        var key = game.worldProperties.isDay ? 'touchday' : 'touchnight';
+                        game.currentLocation.text += game.currentLocation.descriptions[key];
+                    }
+                }
             ]
         }
     }
