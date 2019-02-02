a636aa14f2d3183abb9de456a6c6bc0ff9e09540
Rutger Schurgers <rutger@strixit.com>
2019-02-02 16:06:57 +0100

    Remove html

--- src/Games/QuestForTheKing/items/Frostbite.ts
+++ src/Games/QuestForTheKing/items/Frostbite.ts
@@ -2,7 +2,6 @@
     export function Frostbite(): IItem {
         return {
             name: 'Frostbite Spell',
-            description: StoryScript.Constants.HTML,
             damage: '2',
             equipmentType: StoryScript.EquipmentType.LeftHand,
             value: 5,
