ce60f968e62096d237d072813b24b9f818a24413
Rutger Schurgers <rutger@strixit.com>
2019-02-10 18:10:50 +0100

    Commit before merging to master

---
+++ src/Games/MyInteractiveStory/run.ts
@@ -0,0 +1,4 @@
+namespace MyInteractiveStory {
+    // Calling this function will bootstrap the game using our game namespace and rules and text objects.
+    StoryScript.Run('MyInteractiveStory', new CustomTexts().texts);
+}
