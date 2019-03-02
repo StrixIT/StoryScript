25f44f25c5e12f5c6073fa4c6c1d6219fcd7c548
Cassanne <sanne.jense@gmail.com>
2019-03-02 21:39:58 +0100

    Junglestory

---
+++ src/Games/JungleStory/customTexts.ts
@@ -0,0 +1,14 @@
+namespace JungleStory {
+    export class CustomTexts {
+        texts: StoryScript.IInterfaceTexts = {
+            // Add your custom texts here.
+            gameName: 'Jungle',
+            youAreHere: '{0}',
+            startOver: 'Begin opnieuw',
+            saveGame: 'Opslaan',
+            loadGame: 'Laad spel'
+
+
+        }
+    }
+}
