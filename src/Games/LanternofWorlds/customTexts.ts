f928f4028b8d8f48961371ef03ecd2f9971e9513
Rutger Schurgers <rutger@strixit.com>
2019-08-27 12:56:21 +0200

    Fix location title

--- src/Games/LanternofWorlds/customTexts.ts
+++ src/Games/LanternofWorlds/customTexts.ts
@@ -2,7 +2,7 @@ namespace LanternofWorlds {
     export function CustomTexts(): StoryScript.IInterfaceTexts {
             return {
             // Add your custom texts here.
-            youAreHere: 'Introduction',
+            youAreHere: '{0}',
             gameName: 'Lantern of Worlds',
             currency: 'Mana',
         };
