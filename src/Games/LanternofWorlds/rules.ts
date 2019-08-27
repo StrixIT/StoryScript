2ad4862297fab8faa3d649549c16789385c08fa0
Rutger Schurgers <rutger@strixit.com>
2019-08-27 21:10:04 +0200

    Basic character creation with attributes

--- src/Games/LanternofWorlds/rules.ts
+++ src/Games/LanternofWorlds/rules.ts
@@ -26,7 +26,137 @@ namespace LanternofWorlds {
                 getCreateCharacterSheet: (): StoryScript.ICreateCharacter => {
                     return {
                         steps: [
-                            // Add the character creation steps here, if you want to use character creation.
+                            {
+                                questions: [
+                                    {
+                                        question: 'Do you want to start play...',
+                                        entries: [
+                                            {
+                                                text: 'Right away',
+                                                value: '1'
+                                            },
+                                            {
+                                                text: 'After telling your story',
+                                                value: '2'
+                                            },
+                                            {
+                                                text: 'By fine-tuning your sheet',
+                                                value: '3'
+                                            }
+                                        ]
+                                    }
+                                ],
+                                nextStepSelector: (character, currentStep) => {
+                                    switch (currentStep.questions[0].selectedEntry.value) {
+                                        case '1': {
+                                            return 1;
+                                        };
+                                        case '2': {
+                                            return 2;
+                                        };
+                                        case '3': {
+                                            return 3;
+                                        };
+                                        default: {
+                                            return 0;
+                                        }
+                                    }
+                                }
+                            },
+                            {
+                                questions: [
+                                    {
+                                        question: 'Select your hero',
+                                        entries: [
+                                            {
+                                                text: 'Barlon the Oak',
+                                                value: 'barlon',
+                                                bonus: 1
+                                            },
+                                            {
+                                                text: 'Stella the Quick',
+                                                value: 'stella',
+                                                bonus: 1
+                                            },
+                                            {
+                                                text: 'Jane Orion',
+                                                value: 'jane',
+                                                bonus: 1
+                                            },
+                                            {
+                                                text: 'Marlin Peacemaker',
+                                                value: 'marlin',
+                                                bonus: 1
+                                            }
+                                        ]
+                                    }
+                                ],
+                                nextStepSelector: 4
+                            },
+                            {
+                                questions: [
+                                    {
+                                        question: 'As a child, you were always...',
+                                        entries: [
+                                            {
+                                                text: 'strong in fights',
+                                                value: 'strength',
+                                                bonus: 1
+                                            },
+                                            {
+                                                text: 'a fast runner',
+                                                value: 'agility',
+                                                bonus: 1
+                                            },
+                                            {
+                                                text: 'a curious reader',
+                                                value: 'intelligence',
+                                                bonus: 1
+                                            }
+                                        ]
+                                    }
+                                ],
+                                nextStepSelector: 4
+                            },
+                            {
+                                attributes: [
+                                    {
+                                        question: 'Create your hero ...',
+                                        numberOfPointsToDistribute: 10,
+                                        entries: [
+                                            {
+                                                attribute: 'strength',
+                                                max: 5,
+                                                min: 1
+                                            },
+                                            {
+                                                attribute: 'agility',
+                                                max: 5,
+                                                min: 1
+                                            },
+                                            {
+                                                attribute: 'intelligence',
+                                                max: 5,
+                                                min: 1
+                                            },
+                                            {
+                                                attribute: 'charisma',
+                                                max: 5,
+                                                min: 1
+                                            }
+                                        ]
+                                    }
+                                ],
+                                nextStepSelector: 4
+                            },
+                            {
+                                questions: [
+                                    {
+                                        question: 'Good luck...',
+                                        entries: []
+                                    }
+                                ]
+                            }
                         ]
                     };
                 },
