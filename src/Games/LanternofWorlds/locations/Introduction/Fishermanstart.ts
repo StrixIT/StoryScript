1e06f0a115b2ea77aed1d6182a6af5d10cea2f76
unknown <info@fasoldgames.com>
2019-09-04 10:35:10 +0200

    Lantern of worlds story added 4-9

---
+++ src/Games/LanternofWorlds/locations/Introduction/Fishermanstart.ts
@@ -0,0 +1,26 @@
+namespace LanternofWorlds.Locations {
+    export function Fishermanstart() {
+        return Location({
+            name: 'The great lake',
+            destinations: [
+                
+            ],
+            features: [
+            ],
+            items: [
+            ],
+            enemies: [
+            ],
+            persons: [
+            ],
+            enterEvents: [
+            ],
+            leaveEvents: [
+            ],
+            actions: [
+            ],
+            combatActions: [
+            ],
+        });
+    }
+}
