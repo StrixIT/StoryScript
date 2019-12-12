9a1be084966664bfbf2c0ee86fe299c66c0b181f
Rutger Schurgers <rutger@strixit.com>
2019-12-12 23:03:52 +0100

    remove testgame

--- src/Games/_TestGame/interfaces/action.ts
+++
@@ -1,5 +0,0 @@
-import * as StoryScript from '../../../Engine/Interfaces/storyScript';
-
-export function Action(action: StoryScript.IAction): StoryScript.IAction {
-    return StoryScript.Action(action);
-}
--- src/Games/_TestGame/items/journal.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Item } from '../types';
-import { Enumerations } from '../../../Engine/Interfaces/storyScript'
-
-export function Journal() {
-    return Item({
-        name: 'Joe\'s journal',
-        equipmentType: Enumerations.EquipmentType.Miscellaneous,
-    });
-}
