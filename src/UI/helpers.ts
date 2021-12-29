79e8e61d832459d1758c7e80bb4eaf6e4dfd57e7
Rutger Schurgers <rutger@strixit.com>
2021-12-29 17:11:17 +0100

    Fix issue with missing components folder

--- src/UI/helpers.ts
+++ src/UI/helpers.ts
@@ -3,9 +3,7 @@ const _templates = new Map<string, string>();
 export function getTemplate(componentName: string, defaultTemplate?: any): string {
     if (_templates.size === 0) {
         var componentRegex = /\/[a-z_A-Z]{2,}\.component\.html$/;
-
-        // Todo: when the components folder is missing, this fails!
-        var r = require.context('game/ui/components', false, /.component.html$/);
+        var r = require.context('game/ui', true, /.component.html$/);
 
         r.keys().map(i => {
             var match = componentRegex.exec(i);
