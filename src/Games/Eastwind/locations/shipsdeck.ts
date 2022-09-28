98ca9d2abc2119d47a6ea0126521f2be69784c78
Fasoldgames <info@fasoldgames.com>
2022-09-28 10:07:27 +0200

    Snippet problems

---
+++ src/Games/Eastwind/locations/shipsdeck.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './shipsdeck.html';
+
+export function Shipsdeck() {
+	return Location({
+		name: 'Shipsdeck',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
