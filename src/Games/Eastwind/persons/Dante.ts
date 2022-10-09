f13f57089f761de3edb847637d48a826c85a4787
Fasoldgames <info@fasoldgames.com>
2022-10-09 08:41:42 +0200

    Added Dante

---
+++ src/Games/Eastwind/persons/Dante.ts
@@ -0,0 +1,19 @@
+import { IGame, IPerson, Person } from '../types';
+import description from './Dante.html';
+
+export function Dante() {
+	return Person({
+		name: 'Dante',
+		description: description,
+		hitpoints: 10,
+		items: [
+		],
+		quests: [
+		],
+		canAttack: false,
+		conversation: {
+			actions: {
+			}
+		},
+	});
+}
