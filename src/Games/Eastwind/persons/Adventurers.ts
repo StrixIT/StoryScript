8f20243a4d930d0578b6f5399cd11c03638c73fd
Fasoldgames <info@fasoldgames.com>
2022-10-11 09:18:56 +0200

    added adventurers

---
+++ src/Games/Eastwind/persons/Adventurers.ts
@@ -0,0 +1,19 @@
+import { IGame, IPerson, Person } from '../types';
+import description from './Adventurers.html';
+
+export function Adventurers() {
+	return Person({
+		name: 'Adventurers',
+		description: description,
+		hitpoints: 10,
+		items: [
+		],
+		quests: [
+		],
+		canAttack: false,
+		conversation: {
+			actions: {
+			}
+		},
+	});
+}
