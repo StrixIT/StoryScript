c93b137944402a78234ccbe44d769a98ad4102db
Fasoldgames <info@fasoldgames.com>
2022-10-13 08:58:40 +0200

    Added enemies & items

---
+++ src/Games/Eastwind/locations/ShipsHold.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './ShipsHold.html';
+
+export function ShipsHold() {
+	return Location({
+		name: 'ShipsHold',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
