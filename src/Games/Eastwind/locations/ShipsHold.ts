eb4d2ce0445c24e7d35f59a014f9e02ccfd2dc11
Fasoldgames <info@fasoldgames.com>
2022-10-28 09:06:19 +0200

    Added ShipsHold Aft and Front

--- src/Games/Eastwind/locations/ShipsHold.ts
+++ src/Games/Eastwind/locations/ShipsHold.ts
@@ -1,12 +1,21 @@
 import { IGame, Location } from '../types';
 import description from './ShipsHold.html';
+import { ShipsHoldAft } from './ShipsHoldAft';
+import { ShipsholdFront } from './ShipsholdFront';
 
 export function ShipsHold() {
 	return Location({
 		name: 'ShipsHold',
 		description: description,
 		destinations: [
-			
+			{
+				name: 'Go to the aft of the hold',
+				target: ShipsHoldAft
+			  },
+			  {
+				name: 'Go to the front of the hold',
+				target: ShipsholdFront
+			  },
 		],
 		features: [
 		],
---
+++ src/Games/Eastwind/locations/ShipsHoldAft.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './ShipsHoldAft.html';
+
+export function ShipsHoldAft() {
+	return Location({
+		name: 'ShipsHoldAft',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
