25477a01bfa005fb220c9b069a58ee3d079a56f7
Rutger Schurgers <rutger@strixit.com>
2022-12-03 13:16:28 +0100

    Action description demo, spell slot

--- src/Games/Eastwind/interfaces/spell.ts
+++ src/Games/Eastwind/interfaces/spell.ts
@@ -2,7 +2,7 @@ import { EquipmentType } from "storyScript/Interfaces/storyScript";
 import { IItem, Item } from "./item";
 
 export function Spell(entity: { name: string, description: string }): ISpell {
-    var spell = Item({ ... entity, equipmentType: EquipmentType.Miscellaneous }) as ISpell;
+    var spell = Item({ ... entity, equipmentType: 'Spell' }) as ISpell;
     spell.useInCombat = true;
     spell.isSpell = true;
     return spell;
