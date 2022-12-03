66375f084a32b45535d9f6f237ba42097ff700b5
Rutger Schurgers <rutger@strixit.com>
2022-12-03 13:57:49 +0100

    Use only equipped spell in combat

--- src/Games/Eastwind/interfaces/spell.ts
+++ src/Games/Eastwind/interfaces/spell.ts
@@ -1,9 +1,11 @@
-import { EquipmentType } from "storyScript/Interfaces/storyScript";
+import { IEquipment } from "./equipment";
 import { IItem, Item } from "./item";
 
 export function Spell(entity: { name: string, description: string }): ISpell {
     var spell = Item({ ... entity, equipmentType: 'Spell' }) as ISpell;
-    spell.useInCombat = true;
+    spell.useInCombat = (item: IItem, equipment: IEquipment) => {
+        return equipment.spell === item;
+    };
     spell.isSpell = true;
     return spell;
 }
