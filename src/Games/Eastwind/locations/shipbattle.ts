ac861715bd80bf0d55aaeb6687bec87fb678bf9d
Fasoldgames <info@fasoldgames.com>
2022-12-16 09:36:43 +0100

    16-12

---
+++ src/Games/Eastwind/locations/shipbattle.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './shipbattle.html';
+
+export function Shipbattle() {
+	return Location({
+		name: 'Shipbattle',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
