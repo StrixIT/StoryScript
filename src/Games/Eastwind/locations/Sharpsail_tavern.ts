9f73bae399c3d767993aa4e20f5c895c6fa02e59
Fasoldgames <info@fasoldgames.com>
2022-12-23 11:43:58 +0100

    23-12.2

---
+++ src/Games/Eastwind/locations/Sharpsail_tavern.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './Sharpsail_tavern.html';
+
+export function Sharpsail_tavern() {
+	return Location({
+		name: 'Sharpsail_tavern',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
