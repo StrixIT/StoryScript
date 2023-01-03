b256fbdf952fdacd01b308af48d2b039bea9fbca
Fasoldgames <info@fasoldgames.com>
2023-01-03 11:52:27 +0100

    3-1

--- src/Games/Eastwind/rules.ts
+++ src/Games/Eastwind/rules.ts
@@ -56,7 +56,6 @@ export function Rules(): IRules {
             getCombinationActions: (): ICombinationAction[] => {
                 return [
                     {text: Constants.LOOKAT,
-                        preposition: 'at',
                         requiresTool: false,
                         failText: (game, target, tool): string => {
                             return 'you look at the ' + target.name + '. There is nothing special about it.';
