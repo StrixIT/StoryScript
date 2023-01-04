c846171cbd8fc80e65a16acfddf19cc9b9c3c419
Fasoldgames <info@fasoldgames.com>
2023-01-04 10:41:32 +0100

    4-1

---
+++ src/Games/Eastwind/locations/Junglepond.ts
@@ -0,0 +1,30 @@
+import { IGame, Location } from '../types';
+import description from './Junglepond.html';
+
+export function Junglepond() {
+	return Location({
+		name: 'Junglepond',
+		description: description,
+		destinations: [
+			
+		],
+		features: [
+		],
+		items: [
+		],
+		enemies: [
+		],
+		persons: [
+		],
+		trade: [
+		],
+		enterEvents: [
+		],
+		leaveEvents: [
+		],
+		actions: [
+		],
+		combatActions: [
+		],
+	});
+}
