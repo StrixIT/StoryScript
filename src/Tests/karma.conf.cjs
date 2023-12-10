bcc0e14c21410d7a76dab5e3185b7605e11ceffa
Rutger Schurgers <rutger@strixit.com>
2023-12-10 21:19:31 +0100

    Fix code coverage, no more package vulnerabilities

--- src/Tests/karma.conf.cjs
+++ src/Tests/karma.conf.cjs
@@ -5,6 +5,15 @@ module.exports = function (karmaConfig, webPackConfig, mainJSPath, testPath, gam
   webPackConfig.default.resolve.alias.game = path.resolve(__dirname, gamePath);
   webPackConfig.default.output = { path: path.join(os.tmpdir(), '_karma_webpack_') + Math.floor(Math.random() * 1000000) };
 
+  webPackConfig.default.module.rules.push({
+    enforce: 'post',
+    test: /\.ts$/,
+    use: {
+      loader: 'coverage-istanbul-loader'
+    },
+    include: path.resolve(__dirname, codePath)
+  });
+
   return {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -30,7 +39,16 @@ module.exports = function (karmaConfig, webPackConfig, mainJSPath, testPath, gam
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage-istanbul'],
+
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'text-summary', 'lcovonly' ],
+      dir: path.join(__dirname, testPath, 'coverage'),
+      fixWebpackSourcePaths: true,
+      'report-config': {
+        html: { outdir: 'html' }
+      }
+    },
 
     // web server port
     port: 9876,
