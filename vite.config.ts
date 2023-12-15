c3fdd8aecf573f3c82123b056cc91939b477b6d3
Rutger Schurgers <rutger@strixit.com>
2023-12-15 15:03:23 +0100

    Update vite config to copy resources

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import gameName from './gameName.js';
 import path from 'path';
+import { normalizePath } from 'vite';
+
+const gamePath = path.resolve(__dirname, `./src/Games/${gameName}/`);
 
 const htmlImport = {
   name: "htmlImport",
@@ -20,18 +24,39 @@ const htmlImport = {
   }
 }
 
+const resourceRegex = /\/resources\/(.{1,}\.)/;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+
   define: {
     'process.env': {}
   },
   resolve: {
     alias: {
         storyScript: path.resolve(__dirname, './src/Engine'),
-        game: path.resolve(__dirname, `./src/Games/${gameName}/`)
+        game: gamePath
     }
   },
-  plugins: [tsconfigPaths(), htmlImport],
+  plugins: [
+    tsconfigPaths(),     
+    viteStaticCopy({
+      watch: {
+        reloadPageOnChange: true
+      },
+      targets: [
+        {
+          src: normalizePath(path.resolve(gamePath, 'resources/**/*.*')),
+          dest: 'resources',
+          rename: (_, extension, fullPath) => {
+            var match = fullPath.match(resourceRegex)[1];
+            return match + extension;
+          }
+        }
+      ]
+    }),
+    htmlImport
+  ],
   server: {
     port: 3000,
     open: true
