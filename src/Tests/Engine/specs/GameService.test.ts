145d62a9e7664c75fb85070965c4118a6e03af8e
Rutger Schurgers <rutger@strixit.com>
2024-09-05 16:45:07 +0200

    Engine fixes for initializing a game

--- src/Tests/Engine/specs/GameService.test.ts
+++ src/Tests/Engine/specs/GameService.test.ts
@@ -1,9 +1,17 @@
-import {describe, expect, test} from 'vitest';
+import {beforeAll, describe, expect, test} from 'vitest';
 import {GameService} from 'storyScript/Services/gameService';
-import {EquipmentType, ICharacter, ICompiledLocation, IGame, IItem} from 'storyScript/Interfaces/storyScript';
+import {EquipmentType, ICharacter, ICompiledLocation, IGame, IItem, IRules} from 'storyScript/Interfaces/storyScript';
+import {IDataService} from "storyScript/Interfaces/services/dataService.ts";
+import {ISaveGame} from "storyScript/Interfaces/saveGame.ts";
+import {ILocationService} from "storyScript/Interfaces/services/locationService.ts";
+import {addArrayExtensions} from "storyScript/arrayAndFunctionExtensions.ts";
 
 describe("GameService", function() {
 
+    beforeAll(() => {
+        addArrayExtensions();
+    });
+    
     test("should call the use function on an item", function() {
         let used = false;
         const character = <ICharacter>{};
@@ -53,7 +61,51 @@ describe("GameService", function() {
         expect(character.equipment.leftHand).toBeNull();
         expect(character.items[0]).toEqual(amulet);
     });
-    
+
+    test("should init readonly party properties when loading a game", function() {
+        const character = <ICharacter>{
+            items: []
+        }
+        
+        const game = <IGame>{
+            sounds: {},
+            combinations: {
+                combinationResult: {
+                    reset: () => {}
+                }
+            }
+        };
+        
+        const rules = <IRules>{
+            setup: {}
+        };
+        
+        const saveGame = <ISaveGame>{
+            party: {
+                characters: [
+                    {...character}, {...character}, {...character}
+                ],
+                currentLocationId: 'start'
+            },
+            world: <any>{
+                'start': <ICompiledLocation>{},
+                get: () => <ICompiledLocation>{}
+            }
+        }
+        
+        const dataService = <IDataService>{
+            load: (key) => saveGame
+        };
+        
+        const locationService = <ILocationService>{
+            init: () => {},
+            loadLocationDescriptions: (game) => {}
+        }
+        
+        const service = getService(game, dataService, locationService, null, null, rules);
+        service.loadGame('');
+        game.party.characters.forEach((c) => expect(c.combatItems).toBeDefined());
+    });
 });
 
 function getService(game?, dataService?, locationService?, characterService?, combinationService?, rules?, helperService?, texts?) {
