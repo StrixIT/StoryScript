f0b85cbe18868b39494f0c1109b995cbbb1ccb2e
Rutger Schurgers <rutger@strixit.com>
2025-07-19 17:01:12 +0200

    Add only used items from the CommonModule

--- src/UI/Modules/sharedModule.ts
+++ src/UI/Modules/sharedModule.ts
@@ -1,16 +1,18 @@
 import {SafePipe} from "ui/Pipes/sanitizationPipe.ts";
 import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
+import {NgClass, NgStyle} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
-        SafePipe,
-        CommonModule,
-        FormsModule
+        NgClass,
+        NgStyle,
+        FormsModule,
+        SafePipe
     ],
     exports: [
-        CommonModule,
+        NgClass,
+        NgStyle,
         FormsModule,
         SafePipe
     ]
