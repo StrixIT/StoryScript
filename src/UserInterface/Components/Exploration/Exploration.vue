115f8758c1eeb8090de03861173fd351a4a35c67
RutgerSchurgers <rutger@strixit.com>
2025-12-12 14:07:43 +0100

    Fix barriers

--- src/UserInterface/Components/Exploration/Exploration.vue
+++ src/UserInterface/Components/Exploration/Exploration.vue
@@ -21,17 +21,16 @@
       <div class="box-title">{{ texts.destinations }}</div>
       <ul class="list-unstyled">
         <li v-for="destination of location.activeDestinations" :class="`inline ${destination.visited ? '' : 'not-'}visited`">
-          <div v-for="barrier of destination.barriers?.map(b => b[1])" class="barrier">
-            <button v-if="!barrier.actions?.length" class="btn btn-outline-primary">{{ barrier.name }}</button>
-<!--            <div v-else ngbDropdown class="d-inline-block" class="action-select">-->
-<!--              <button class="btn btn-outline-primary" id="barrierdropdown" ngbDropdownToggle>{{ barrierEntry.name }}</button>-->
-<!--              <div ngbDropdownMenu aria-labelledby="barrierdropdown">-->
-<!--                @for (action of barrier[1].actions; track action)-->
-<!--                {-->
-<!--                <button ngbDropdownItem (click)="executeBarrierAction(barrier, action, destination)">{{ action[1].text }}</button>-->
-<!--                }-->
-<!--              </div>-->
-<!--            </div>-->
+          <div v-for="barrier of destination.barriers" class="barrier">
+            <button v-if="!barrier[1].actions?.length" class="btn btn-outline-primary">{{ barrier[1].name  }}</button>
+            <div v-else class="dropdown">
+              <button class="btn btn-outline-primary dropdown-toggle" type="button" @click="dropDownExpanded = !dropDownExpanded" :aria-expanded="dropDownExpanded">
+                {{ barrier[1].name }}
+              </button>
+              <ul class="dropdown-menu action-select" :class="dropDownMenuActive" v-if="dropDownExpanded">
+                <li v-for="action of barrier[1].actions"><a class="dropdown-item" href="#" @click="executeBarrierAction(barrier, action, destination)">{{ action[1].text }}</a></li>
+              </ul>
+            </div>
           </div>
           <button type="button" class="btn btn-info" :class="destination.style" @click="changeLocation(<string>destination.target)" :disabled="!destination.target || destination.barriers?.length > 0">
             <span v-if="(<any>destination).isPreviousLocation" class="back-label">{{ texts.back }}</span>
@@ -48,15 +47,21 @@ import {storeToRefs} from "pinia";
 import {enemiesPresent, getButtonClass, executeAction} from "vue/Helpers.ts";
 import {isEmpty} from "storyScript/utilityFunctions.ts";
 import {IAction} from "storyScript/Interfaces/action.ts";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {ActionStatus} from "storyScript/Interfaces/enumerations/actionStatus.ts";
 import {ICompiledLocation} from "storyScript/Interfaces/compiledLocation.ts";
+import {IBarrier} from "storyScript/Interfaces/barrier.ts";
+import {IBarrierAction} from "storyScript/Interfaces/barrierAction.ts";
+import {IDestination} from "storyScript/Interfaces/destination.ts";
 
 const store = useStateStore();
 const {game} = storeToRefs(store);
 const {texts, dataService} = store.services;
 
+const dropDownExpanded = ref(false);
 const confirmAction = ref<IAction>(null);
+
+const dropDownMenuActive = computed(() => dropDownExpanded.value ? 'dropdown-menu-active' : '');
 
 const { location } = defineProps<{
   location?: ICompiledLocation
@@ -72,6 +77,16 @@ const actionsPresent = () => location && !enemiesPresent(game.value) && !isEmpty
 const disableActionButton = (a) => checkStatus(a, ActionStatus.Disabled);
 
 const cancelAction = () => confirmAction.value = undefined;
+
+const executeBarrierAction = (barrier: [string, IBarrier], action: [string, IBarrierAction], destination: IDestination): void => {
+  if (game.value.combinations.tryCombine(barrier[1]).success || game.value.combinations.activeCombination) {
+    return;
+  }
+
+  action[1].execute(game.value, barrier, destination);
+  barrier[1].actions.delete(barrier[1].actions.find(([k, _]) => k === action[0]));
+  dataService.saveGame(game.value);
+}
 
 const execute = (action: IAction): void => {
   if (action[1].confirmationText && !confirmAction) {
