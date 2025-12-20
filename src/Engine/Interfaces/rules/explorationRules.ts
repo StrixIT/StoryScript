958291dee6fc6c8c21819560a5040dcf55ca20d2
RutgerSchurgers <rutger@strixit.com>
2025-12-20 13:23:39 +0100

    Remove duplicated method

--- src/Engine/Interfaces/rules/explorationRules.ts
+++ src/Engine/Interfaces/rules/explorationRules.ts
@@ -21,16 +21,6 @@ export interface IExplorationRules {
      */
     leaveLocation?(game: IGame, location: ICompiledLocation, newLocationId: string): void;
 
-    hasVisitedLocation?(game: IGame, location: ICompiledLocation): boolean;
-
-    /**
-     * When specified, this function will be called to determine whether a destination should
-     * be marked as visited.
-     * @param game The active game
-     * @param location The location the destination leads to
-     */
-    hasVisitedLocation?(game: IGame, location: ICompiledLocation): boolean;
-
     /**
      * When specified, this function will be called to determine whether a destination should
      * be marked as visited.
