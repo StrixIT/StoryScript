9fb88834d235c8ec121af62ecc04622684694388
RutgerSchurgers <rutger@strixit.com>
2025-12-27 17:43:46 +0100

    Simplify equipment component

--- src/UI/Components/Party/Equipment.vue
+++ src/UI/Components/Party/Equipment.vue
@@ -1,48 +1,22 @@
 <template>
   <div v-if="store.showEquipment(character)" class="character-equipment box-container">
-    <collapsible :text="texts.equipmentHeader" :headerClass="'box-title'">
+    <collapsible :headerClass="'box-title'" :text="texts.equipmentHeader">
       <div class="equipment-panel">
         <equipment-slot slot="head" :character="character"></equipment-slot>
         <equipment-slot slot="amulet" :character="character"></equipment-slot>
         <div class="row">
-          <div class="col-3">
-            <span v-if="isSlotUsed(['hands', 'rightHand', 'rightRing'])">{{ texts.rightHand }}</span>
+          <div class="col-4">
+            <equipment-slot slot="hands" :character="character"></equipment-slot>
+            <equipment-slot slot="rightHand" :character="character"></equipment-slot>
+            <equipment-slot slot="rightRing" :character="character"></equipment-slot>
           </div>
-          <div class="col-6">
-            <span v-if="isSlotUsed('body')">{{ texts.body }}</span>
+          <div class="col-4 body-slot">
+            <equipment-slot slot="body" :character="character"></equipment-slot>
           </div>
-          <div class="col-3">
-            <span v-if="isSlotUsed(['hands', 'leftHand', 'leftRing'])">{{ texts.leftHand }}</span>
-          </div>
-          <div class="col-3">
-            <div v-if="isSlotUsed('hands')" class="item-box" @click="unequipItem(character.equipment.hands)">
-              {{ getItemName(character.equipment.hands) }}
-            </div>
-            <div v-if="isSlotUsed('rightHand')" :class="{ 'side-borders' : isSlotUsed(['hands', 'rightRing']) }"
-                 class="item-box"
-                 @click="unequipItem(character.equipment.rightHand)">{{ getItemName(character.equipment.rightHand) }}
-            </div>
-            <div v-if="isSlotUsed('rightRing')" class="item-box" @click="unequipItem(character.equipment.rightRing)">
-              {{ getItemName(character.equipment.rightRing) }}
-            </div>
-          </div>
-          <div class="col-6">
-            <div v-if="isSlotUsed(['rightHand', 'leftHand']) && isSlotUsed('hands')" class="item-box no-border"></div>
-            <div v-if="isSlotUsed('body')" class="item-box" @click="unequipItem(character.equipment.body)">
-              {{ getItemName(character.equipment.body) }}
-            </div>
-          </div>
-          <div class="col-3">
-            <div v-if="isSlotUsed('hands')" class="item-box" @click="unequipItem(character.equipment.hands)">
-              {{ getItemName(character.equipment.hands) }}
-            </div>
-            <div v-if="isSlotUsed('leftHand')" :class="{ 'side-borders' : isSlotUsed(['hands', 'leftRing']) }"
-                 class="item-box"
-                 @click="unequipItem(character.equipment.leftHand)">{{ getItemName(character.equipment.leftHand) }}
-            </div>
-            <div v-if="isSlotUsed('leftRing')" class="item-box" @click="unequipItem(character.equipment.leftRing)">
-              {{ getItemName(character.equipment.leftRing) }}
-            </div>
+          <div class="col-4">
+            <equipment-slot slot="hands" :character="character"></equipment-slot>
+            <equipment-slot slot="leftHand" :character="character"></equipment-slot>
+            <equipment-slot slot="leftRing" :character="character"></equipment-slot>
           </div>
         </div>
         <equipment-slot slot="legs" :character="character"></equipment-slot>
@@ -56,12 +30,11 @@
 import {useStateStore} from "ui/StateStore.ts";
 import {storeToRefs} from "pinia";
 import {DefaultEquipment} from "storyScript/Interfaces/defaultEquipment.ts";
-import {IItem} from "storyScript/Interfaces/item.ts";
 import {ICharacter} from "storyScript/Interfaces/character.ts";
 
 const store = useStateStore();
 const {useEquipment} = storeToRefs(store);
-const {texts, itemService, characterService} = store.services;
+const {texts} = store.services;
 
 const {character} = defineProps<{
   character?: ICharacter
@@ -73,14 +46,5 @@ const customSlots = (): string[] => {
   const defaultSlots = Object.keys(new DefaultEquipment());
   return Object.keys(character.equipment).filter(e => defaultSlots.indexOf(e) === -1)
 };
-
-const getItemName = (item: IItem): string => itemService.getItemName(item);
-
-const unequipItem = (item: IItem): boolean => itemService.unequipItem(character, item);
-
-const isSlotUsed = (slot: string | string[]): boolean => {
-  const slots = Array.isArray(slot) ? slot : [slot];
-  return slots.find(s => characterService.isSlotUsed(character, s)) !== undefined;
-}
 
 </script>
