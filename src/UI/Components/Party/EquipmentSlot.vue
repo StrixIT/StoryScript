1d33df70e4dbcf0a3d7481566a0aba4bf8afcf71
RutgerSchurgers <rutger@strixit.com>
2025-12-31 14:30:16 +0100

    Inline option for equipment slot

--- src/UI/Components/Party/EquipmentSlot.vue
+++ src/UI/Components/Party/EquipmentSlot.vue
@@ -1,11 +1,11 @@
 <template>
   <div v-if="isSlotUsed(slot)" class="row">
     <div class="col-12">
-      <div>
+      <div v-if="!inline">
         {{ texts[slot] }}
       </div>
-      <div class="item-box" @click="itemService.unequipItem(character, character.equipment[slot])">
-        {{ itemService.getItemName(character.equipment[slot]) }}
+      <div class="item-box" @click="itemService.unequipItem(character, character.equipment[slot])" :class="!inline || character.equipment[slot] ? '' : 'empty-slot'">
+        {{ itemService.getItemName(character.equipment[slot]) ?? (inline ? texts[slot] : null) }}
       </div>
     </div>
   </div>
@@ -18,8 +18,9 @@ const store = useStateStore();
 const {texts, itemService, characterService} = store.services;
 
 const {character, slot} = defineProps<{
-  character: ICharacter
-  slot: string
+  character: ICharacter;
+  slot: string;
+  inline?: boolean;
 }>();
 
 const isSlotUsed = (slot: string | string[]): boolean => {
