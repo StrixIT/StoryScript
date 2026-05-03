64e7136eaeb54e7a1e056a4276069c4d4ad643a0
Monika Barget <38257338+MonikaBarget@users.noreply.github.com>
2026-05-03 15:10:35 +0200

    Enhance Luxembourg location with description and destinations
    Added description and destinations to Luxembourg location.

--- src/Games/MyInteractiveMap/locations/Luxembourg.ts
+++ src/Games/MyInteractiveMap/locations/Luxembourg.ts
@@ -2,6 +2,8 @@ import {Location} from '../types';
 
 export function Luxembourg() {
     return Location({
-        name: 'Luxembourg'
+        name: 'Luxembourg',
+        description: " ",
+        destinations: []
     });
 }
